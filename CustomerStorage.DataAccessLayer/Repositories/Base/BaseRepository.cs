d5424e20b73873f440c0bd3a7fe3aa1699e63689
Yaroslav Homela <yho@compent.net>
2023-02-10 00:37:08 +0200

    Add customer repository

---
+++ CustomerStorage.DataAccessLayer/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿
+namespace CustomerStorage.DataAccessLayer.Repositories.Base
+{
+    public class BaseRepository<TEntity> where TEntity : Entities.Base.Base
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task Create(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public virtual async Task CreateRange(List<TEntity> entities)
+        {
+            await _context.AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<TEntity> ReadById(int id)
+        {
+            var entity = await _context.FindAsync<TEntity>(id);
+            return entity;
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public virtual async Task UpdateRange(List<TEntity> entities)
+        {
+            _context.UpdateRange(entities);
+            await _context.SaveChangesAsync();
+        }
+        public virtual async Task Delete(int id)
+        {
+            Entities.Base.Base entity = new Entities.Base.Base() { Id = id };
+            _context.Attach(entity);
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CustomerStorage.DataAccessLayer/Repositories/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using CustomerStorage.DataAccessLayer.Entities;
+using CustomerStorage.DataAccessLayer.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerStorage.DataAccessLayer.Repositories
+{
+    public class CustomerRepository : BaseRepository<Customer>
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomerRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<Customer>> GetCustomers()
+        {
+            var customers = await _context.Customers
+                .AsNoTracking()
+                .ToListAsync();
+            return customers;
+        }
+    }
+}
