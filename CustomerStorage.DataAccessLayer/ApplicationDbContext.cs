faabaecd91780ec26099b457f7f8cfc7a58fb20e
Yaroslav Homela <yho@compent.net>
2023-02-17 14:32:49 +0200

    Add clustered index to customer name

--- CustomerStorage.DataAccessLayer/ApplicationDbContext.cs
+++ CustomerStorage.DataAccessLayer/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using CustomerStorage.DataAccessLayer.Entities;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Reflection.Emit;
+using System;
 
 namespace CustomerStorage.DataAccessLayer
 {
@@ -16,8 +19,9 @@ namespace CustomerStorage.DataAccessLayer
             base.OnModelCreating(builder);
 
             builder.Entity<Customer>()
-                .HasIndex(u => u.Name)
-            .IsUnique();
+            .HasIndex(u => u.Name)
+            .IsUnique()
+            .IsClustered();
 
             builder.Entity<Customer>().HasQueryFilter(e => !e.IsRemoved);
         }
---
+++ CustomerStorage.DataAccessLayer/Migrations/20230217122953_add_name_index.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerStorage.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnameindex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Name",
+                table: "Customers",
+                column: "Name",
+                unique: true)
+                .Annotation("SqlServer:Clustered", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Name",
+                table: "Customers");
+        }
+    }
+}
